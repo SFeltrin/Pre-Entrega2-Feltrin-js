ee5ec362bde8f72449a764222786770324666a64
sfeltrin <seba.feltrin@gmail.com>
2023-05-22 13:05:55 -0300

    se agrega bucle para recorrer el array Platos antes del filtro

--- js/main.js
+++ js/main.js
@@ -1,53 +1,45 @@
 const platos = [
-    { id: 1, nombre: "Hamburguesa", precio: 1000 },
-    { id: 2, nombre: "Pizza", precio: 1500 },
-    { id: 3, nombre: "Sushi", precio: 2000 },
-    { id: 4, nombre: "Ensalada", precio: 980 },
-    { id: 5, nombre: "Pasta", precio: 1200 }
-  ];
-  
+  { id: 1, nombre: "Hamburguesa", precio: 1000 },
+  { id: 2, nombre: "Pizza", precio: 1500 },
+  { id: 3, nombre: "Sushi", precio: 2000 },
+  { id: 4, nombre: "Ensalada", precio: 980 },
+  { id: 5, nombre: "Pasta", precio: 1200 }
+];
+
 // Función para crear un nuevo plato
 function crearPlato() {
-    // Solicitar al usuario los detalles del nuevo plato
-    let nombre = prompt("Ingrese el nombre del nuevo plato:");
-    let precio = parseFloat(prompt("ahora ingrese el precio:"));
-  
-    // Crear un objeto para el nuevo plato
-    let nuevoPlato = {
-      id: platos.length + 1,
-      nombre: nombre,
-      precio: (precio),
-    }
-  
-    // Agregar nuevo plato al menu
-    platos.push(nuevoPlato);
+  let nombre = prompt("Ingrese el nombre del nuevo plato:");
+  let precio = parseFloat(prompt("Ingrese el precio del nuevo plato:"));
 
-    console.log(platos)
+  let nuevoPlato = {
+    id: platos.length + 1,
+    nombre: nombre,
+    precio: precio
   }
-  
-  // Llamar a la función para crear un nuevo plato
-  crearPlato();
 
+  platos.push(nuevoPlato);
+}
 
+// Llamar a la función para crear un nuevo plato
+crearPlato();
+
+// Recorrer y mostrar los platos
+for (let i = 0; i < platos.length; i++) {
+  console.log("id: " + platos[i].id);
+  console.log("nombre: " + platos[i].nombre);
+  console.log("precio: " + platos[i].precio);
+}
 
 // Filtrar los platos por precio
 function filtrarPorPrecio() {
-    // Solicitar al usuario el precio del producto que quiere buscar
-    let filtroPrecio = parseFloat(prompt("Ingrese el precio del producto para filtrar:"));
-  
-    // Fitro 
-    let platosFiltrados = platos.filter((plato) => {
-      if (plato.precio == filtroPrecio) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  
-    console.log(platosFiltrados);
-  }
-  
-  // Llamar a la función para realizar el filtro
-  filtrarPorPrecio();
-  
-  
+  let filtroPrecio = parseFloat(prompt("Ingrese el precio del producto para filtrar:"));
+
+  let platosFiltrados = platos.filter((plato) => {
+    return plato.precio <= filtroPrecio;
+  });
+
+  console.log(platosFiltrados);
+}
+
+// Llamar a la función para realizar el filtro
+filtrarPorPrecio();
